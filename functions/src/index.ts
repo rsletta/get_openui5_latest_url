4010295bb178351874c32b3f87508b81dd7e1f24
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2019-04-28 10:58:13 +0200

    Refactor response format

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,47 +19,51 @@ main.use(bodyParser.urlencoded({ extended: false }));
 app.get('/latest', (req: any, res: any) => {
     const latestRef = db.collection('versions').doc('latest');
     latestRef.get()
-           .then(doc => {
-               if (!doc.exists) {
-                   console.log("Document didn't exist");
-                   return;
-               } else {
-                   return doc.data();
-               }
-           }).then(doc => {
-               const type = req.query.type;
-               let resData = ""; 
-               if (doc !== undefined) {
-                   resData =  doc[type]
-               }
-               res.send(resData);
-           })
-           .catch(err => {
-               console.log( 'Error getting document:', err);
-           })
+        .then(doc => {
+            if (!doc.exists) {
+                console.error("Document not found.");
+                return res.status(404).send("Document not found");
+            } else {
+                return doc.data();
+            }
+        }).then(doc => {
+            const type = req.query.type;
+
+            const resData = {
+                'version': doc.version,
+                'type': type,
+                'url': doc[type]
+            }
+            res.send(resData);
+        })
+        .catch(err => {
+            console.log('Error getting document:', err);
+        })
 });
 
 app.get('/latest/:id', (req: any, res: any) => {
     const latestRef = db.collection('versions').doc(req.params.id);
     latestRef.get()
-           .then(doc => {
-               if (!doc.exists) {
-                   console.error("Document not found. Version: " + req.params.id);
-                   return res.status(404).send("Version not found");
-               } else {
-                   return doc.data();
-               }
-           }).then(doc => {
-               const type = req.query.type;
-               let resData = ""; 
-               if (doc !== undefined) {
-                   resData =  doc[type]
-               }
-               res.send(resData);
-           })
-           .catch(err => {
-               console.log( 'Error getting document:', err);
-           })
+        .then(doc => {
+            if (!doc.exists) {
+                console.error("Document not found. Version: " + req.params.id);
+                return res.status(404).send("Version not found");
+            } else {
+                return doc.data();
+            }
+        }).then(doc => {
+            const type = req.query.type;
+
+            const resData = {
+                'version': doc.version,
+                'type': type,
+                'url': doc[type]
+            }
+            res.send(resData);
+        })
+        .catch(err => {
+            console.log('Error getting document:', err);
+        })
 });
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
